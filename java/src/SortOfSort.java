16414f04f0acf48be0f28579ae360a7573644de2
Matthew <mdherrera3@miners.utep.edu>
2020-03-02 14:46:38 -0700

    started on the project and still need to figure out the sorting

---
+++ java/src/SortOfSort.java
@@ -0,0 +1,8 @@
+public class SortOfSort {
+    public static void sortOfSort(int[] array){
+        int temp;
+        for(int i = 0; i < array.length; i++){
+            int counter = 0;
+        }
+    }
+}
