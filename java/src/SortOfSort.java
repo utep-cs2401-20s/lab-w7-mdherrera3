4938b9ebd8e89dc8e3f52c68a9133f7e3a268514
Matthew <mdherrera3@miners.utep.edu>
2020-03-04 14:49:15 -0700

    almost done with keeping track with the sorted array

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -1,8 +1,19 @@
 public class SortOfSort {
     public static void sortOfSort(int[] array){
-        int temp;
-        for(int i = 0; i < array.length; i++){
-            int counter = 0;
+        int beginning = 0;
+        int max = array[0];
+        int end = array.length - 1;
+        int counter = 0;
+        while(end !=  beginning) {
+            for(int i = end; i > beginning; i--){
+                if(array[i] > max){
+                    max = array[i];
+                }
+            }
+            if(counter % 2 == 0 ){
+
+            }
+            counter++;
         }
     }
 }
