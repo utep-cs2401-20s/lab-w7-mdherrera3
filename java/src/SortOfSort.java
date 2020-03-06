3e39cdefcb8c9db0f61b0db6ea4cc40f22e1ae47
Matthew <mdherrera3@miners.utep.edu>
2020-03-06 12:38:30 -0700

    Final version of files

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -2,18 +2,51 @@ public class SortOfSort {
     public static void sortOfSort(int[] array){
         int beginning = 0;
         int max = array[0];
-        int end = array.length - 1;
+        int end = array.length;
         int counter = 0;
+        int index = 0;
+        boolean start = false;
+        boolean temp;
+        boolean ending = true;
         while(end !=  beginning) {
-            for(int i = end; i > beginning; i--){
+            max = array[beginning];
+            for(int i = beginning; i < end - 1; i++){
                 if(array[i] > max){
                     max = array[i];
+                    index = i;
                 }
             }
-            if(counter % 2 == 0 ){
-
+            if(counter % 2 == 0) {
+                temp = ending;
+                ending = start;
+                start = temp;
+                if(start){
+                    array[index] = array[beginning];
+                    array[beginning] = max;
+                    beginning = beginning + 1;
+                }
+                if (ending) {
+                    array[index] =  array[end-1];
+                    array[end-1] = max;
+                    end = end - 1;
+                }
+            }
+            else{
+                if(start){
+                    array[index] = array[beginning];
+                    array[beginning] = max;
+                    beginning = beginning + 1;
+                }
+                if (ending) {
+                    array[index] = array[end-1];
+                    array[end-1] = max;
+                    end = end - 1;
+                }
             }
             counter++;
+        }
+        for(int j = 0; j < array.length;j++){
+            System.out.print(array[j] + " ");
         }
     }
 }
--- java/src/SortOfSortTester.java
+++ java/src/SortOfSortTester.java
@@ -1,3 +1,52 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class SortOfSortTester {
+    @Test
+    public void sortTest1(){
+        //simple Test
+        int[] a = new int[] {0,2,4,5,1,7};
+        int[] b = new int[] {4,2,1,0,5,7};
+        SortOfSort sol = new SortOfSort();
+        sol.sortOfSort(a);
+        assertArrayEquals(a,b);
+    }
+    @Test
+    public void sortTest2(){
+        //duplicates
+        int[] a = new int[] {4,2,3,4};
+        int[] b = new int[] {2,1,4,4};
+        SortOfSort sol = new SortOfSort();
+        sol.sortOfSort(a);
+        assertArrayEquals(a,b);
+    }
+    @Test
+    public void sortTest3(){
+        //empty array
+        int[] a = new int[] {};
+        int[] b = new int[] {};
+        SortOfSort sol = new SortOfSort();
+        sol.sortOfSort(a);
+        assertArrayEquals(a,b);
+    }
+    @Test
+    public void sortTest4(){
+        //negatives
+        int[] a = new int[] {-4,-2,-3,-1};
+        int[] b = new int[] {-3,-4,-2,-1};
+        SortOfSort sol = new SortOfSort();
+        sol.sortOfSort(a);
+        assertArrayEquals(a,b);
+    }
+    @Test
+    public void sortTest5(){
+        //negatives and positive and duplicates
+        int[] a = new int[] {-7,-7,4,-7};
+        int[] b = new int[] {-7,-7,-7,4};
+        SortOfSort sol = new SortOfSort();
+        sol.sortOfSort(a);
+        assertArrayEquals(a,b);
+    }
 
 }
